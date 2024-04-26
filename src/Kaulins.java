889c17037b49ed85f124c444174f59ec236b4d00
Daniels <d.jaunarajsjanvaris@gmail.com>
2024-04-26 08:48:24 +0300

    Programma atrod lielāko no uzkritušajiem skaitļiem

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -4,12 +4,16 @@ import java.util.Random;
 public class Kaulins {
 	
 	static void mestKaulinu(int reizes) {
+		int lielakais =0;
 		Random rand = new Random();
 		
 		for(int i = 0; i < reizes; i++) {
 			int skaitlis = rand.nextInt(6) + 1;
 			System.out.println("Uzkrita skaitlis: " + skaitlis);
+			if(lielakais<skaitlis)
+				lielakais = skaitlis;
 		}
+		System.out.print("Lielākais skaitlis no visiem metieniem: "+lielakais);
 	}
 	
 	public static void main(String[] args) {
